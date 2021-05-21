def36fad4e23ebb80a3454ff7c9f63ec703485a7
Nitin Singh <singh.nitinkumar.nitin1995@gmail.com>
2021-05-21 19:42:44 +0530

    First Commit

---
+++ resources/views/components/product.blade.php
@@ -0,0 +1,3 @@
+<div class="w-300 h-300 bg-red">
+
+</div>
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('guest.welcome');
+});
+
+
+Route::get('/quotation', function () {
+    return 'Hello World';
+});
+
+Route::get('/product' , function(){
+    return view('guest.product.index');
+});
+
+
+
+Route::get('/orders', function () {
+    return view('orders');
+});
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
